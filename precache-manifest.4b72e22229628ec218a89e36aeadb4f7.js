dc6e17576cac8bdd9c1673057138deb64ceeef50
Priyankara Saman Wijethunga <74031718+PriyankaraUser@users.noreply.github.com>
2021-06-12 01:24:33 +0530

    Updates

---
+++ precache-manifest.4b72e22229628ec218a89e36aeadb4f7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b413065889466d5930491e218056e556",
+    "url": "/index.html"
+  },
+  {
+    "revision": "facc5ee398b262d020e9",
+    "url": "/static/css/main.956fad23.chunk.css"
+  },
+  {
+    "revision": "94833f0d9aaa3be6210f",
+    "url": "/static/js/2.19e99e5d.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.19e99e5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "facc5ee398b262d020e9",
+    "url": "/static/js/main.c5e36c86.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "fc1fc12a2a302e28c4f6f4d244fbaa28",
+    "url": "/static/media/priyankara.fc1fc12a.png"
+  },
+  {
+    "revision": "4dc9e81c369bc6ecc169f5591c66ba90",
+    "url": "/static/media/resume.4dc9e81c.pdf"
+  }
+]);
